1fa0ee4e7483dc1a74bf243ce9a2b6a2007d0803
AWolf81 <awolf2904@gmail.com>
2017-05-04 23:03:43 +0200

    added checkboxes.

--- js/board-bar.js
+++ js/board-bar.js
@@ -1,5 +1,5 @@
 /* global TrelloPowerUp */
-
+var DEFAULT_STATE_CHECKBOXES = true;
 var t = TrelloPowerUp.iframe();
 
 t.render(function(){
@@ -7,5 +7,14 @@ t.render(function(){
   // and then called each time something changes that
   // you might want to react to, such as new data being
   // stored with t.set()
+  var boardJson = t.args[1];
+  var $checkboxContainer = $('<div><input type="checkbox"><span></span></div>');
+  for(var list in boardJson.lists) {
+      $checkboxContainer.find('input').prop('id', list.id);
+      $('#selectedLists').append($checkboxContainer.clone());
+  }
+
+  $('body').find('input').prop('checked', DEFAULT_STATE_CHECKBOXES);
+
   console.log(t.args, arguments);
 });
